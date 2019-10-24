947b964fe04b997d86a3ca6462993cd286aa3b98
DMatvienko <dmatvienko@griddynamics.com>
2019-10-24 15:36:55 +0400

    fix: paths and rights

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,9 +4,11 @@ frontend_target="FRONTEND"
 backend_target="BACKEND"
 
 if [ "$TARGET_APP" == "$frontend_target" ]; then
-  bash ./frontend.sh
+  bash ./deployment/frontend.sh
 else 
   if [ "$TARGET_APP" == "$backend_target" ]; then
-    bash ./backend.sh
+    bash ./deployment/backend.sh
   else
     echo "Target is not specified. Please see /deployment/README.md"
+  fi
+fi
