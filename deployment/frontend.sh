5ff4dcbe0267f9d431df95430f7ef680558b841c
DMatvienko <dmatvienko@griddynamics.com>
2019-10-24 19:52:39 +0400

    fix: turns off production mode in npm install for using local ng cli

--- deployment/frontend.sh
+++ deployment/frontend.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd frontend
-npm install
+
+#
+# Flag --production=false is needed for using
+#   ng-cli local for current package
+#
+npm install --production=false
+
 npm run build:prod
 npx serve dist/frontend --single --no-clipboard -l $PORT
