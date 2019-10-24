e19340d40815e05b5659a08eba2acdcd1afe99e5
DMatvienko <dmatvienko@griddynamics.com>
2019-10-24 21:27:08 +0400

    fix

--- deployment/frontend.sh
+++ deployment/frontend.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-cd frontend
-
-#
-# Flag --production=false is needed for using
-#   ng-cli local for current package
-#
-npm install --production=false
-
-npm run build:prod
-npx serve dist/frontend --single --no-clipboard -l $PORT
+npx serve frontend/dist/frontend --single --no-clipboard
