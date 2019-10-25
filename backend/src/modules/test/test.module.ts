e0285f58fdc2cb7832b2fa7b2db4cf8205797427
DMatvienko <dmatvienko@griddynamics.com>
2019-10-25 13:36:54 +0400

    creates clothes service

--- backend/src/modules/test/test.module.ts
+++ backend/src/modules/test/test.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClothesController } from './controllers/clothes/clothes.controller';
+import { ClothesService } from './services/clothes/clothes.service';
 
 @Module({
-  controllers: [ClothesController]
+  controllers: [ClothesController],
+  providers: [ClothesService]
 })
 export class TestModule {}
