bc618b3c1dda73d47d5fd39396cd5df8d1080c77
DMatvienko <dmatvienko@griddynamics.com>
2019-10-29 18:11:15 +0400

    feat: writing to console current mode of app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { mode, Mode } from './environment';
 
 async function bootstrap() {
+  const modeName = mode === Mode.Production
+    ? 'production'
+    : 'development';
+  console.log(`App started in ${modeName} mode`);
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
