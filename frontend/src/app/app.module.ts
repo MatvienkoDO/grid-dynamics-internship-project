843bfbc3032e92a38515d3f737e5ae584a8a282b
DMatvienko <dmatvienko@griddynamics.com>
2019-10-30 19:22:05 +0400

    feat: sale component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { SaleComponent } from './shared/components/sale/sale.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
