928d7dd7178b8313e1750a7cf7977697633e12af
jkozeeva <jkozeeva@griddynamics.com>
2019-10-31 15:14:23 +0400

    extra items removes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { CardsComponent } from './shared/components/cards/cards.component';
-import { NewArrivalsComponent } from './pages/home/components/new-arrivals/new-arrivals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CardsComponent,
-    NewArrivalsComponent
+    CardsComponent
   ],
   imports: [
     BrowserModule,
