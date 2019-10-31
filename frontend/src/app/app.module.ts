91f53bd082cc084ded01ff0ad9c4d158ad5b5445
jkozeeva <jkozeeva@griddynamics.com>
2019-10-31 15:08:32 +0400

    adds card component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { CardsComponent } from './shared/components/cards/cards.component';
+import { NewArrivalsComponent } from './pages/home/components/new-arrivals/new-arrivals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CardsComponent,
+    NewArrivalsComponent
   ],
   imports: [
     BrowserModule,
