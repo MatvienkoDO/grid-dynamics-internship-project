4f4e97df984e2f45e3b2fb825d1e03cf907c5e6d
Dmitriy Matvienko <chastor97@gmail.com>
2019-10-31 15:52:46 +0400

    advertisement block (#51)
    * feat: sale component
    
    * fix: renames variable
    
    * feat: advertisement component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { AdvertisementComponent } from './shared/components/advertisement/advertisement.component';
+import { SaleComponent } from './shared/components/sale/sale.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdvertisementComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
