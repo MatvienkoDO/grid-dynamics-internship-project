738c8797d8c7214ba09d6ab19ba2588b105049c3
juliakozeeva <41913104+juliakozeeva@users.noreply.github.com>
2019-10-31 16:39:32 +0400

    Adds slider component without animation (#54)
    * adds slider component without animation
    
    * fixes app component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { SliderComponent } from './pages/slider/slider.component';
 import { CardComponent } from './shared/components/card/card.component';
 import { AdvertisementComponent } from './shared/components/advertisement/advertisement.component';
 import { SaleComponent } from './shared/components/sale/sale.component';
@@ -14,6 +15,7 @@ import { SaleComponent } from './shared/components/sale/sale.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    SliderComponent,
     CardComponent,
     AdvertisementComponent,
     SaleComponent,
