f161ddf145b710e5c620813eb1ed3a017e123848
jkozeeva <jkozeeva@griddynamics.com>
2019-11-01 22:33:41 +0400

    media for cards

--- frontend/src/app/pages/best-sales/best-sales.component.ts
+++ frontend/src/app/pages/best-sales/best-sales.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-best-sales',
   templateUrl: './best-sales.component.html',
-  styleUrls: ['./best-sales.component.scss']
+  styleUrls: ['./best-sales.component.scss'],
 })
 export class BestSalesComponent implements OnInit {
   constructor() { }
