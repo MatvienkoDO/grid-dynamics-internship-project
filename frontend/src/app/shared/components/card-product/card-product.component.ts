726f0ecd5adb0788b3441c3169eac5ae286de811
DMatvienko <dmatvienko@griddynamics.com>
2019-11-01 13:53:27 +0400

    feat: makes card component from card (partially)

--- frontend/src/app/shared/components/card-product/card-product.component.ts
+++ frontend/src/app/shared/components/card-product/card-product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-card-product',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-product.component.scss']
 })
 export class CardProductComponent implements OnInit {
+  @Input('image-url') public readonly imageUrl: string;
+  @Input('title') public readonly title: string;
+  @Input('price') public readonly price: number;
+  @Input('currency-sign') public readonly currencySign: string;
+  @Input('sizes') public readonly sizes: string[];
+  @Input('colors') public readonly colors: string[];
+
+  public readonly size$ = new BehaviorSubject(undefined);
+  public readonly color$ = new BehaviorSubject(undefined);
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public readonly changeSize = size => {
+    this.size$.next(size);
+  }
+
+  public readonly changeColor = color => {
+    this.color$.next(color);
+  }
 }
