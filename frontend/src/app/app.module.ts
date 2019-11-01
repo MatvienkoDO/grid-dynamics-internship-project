92c741657fbeb958247ff9808b7de22746ea9065
juliakozeeva <41913104+juliakozeeva@users.noreply.github.com>
2019-11-01 10:24:53 +0400

    Feature/frontend best sales (#63)
    * replaced card on product card
    
    * optimization for property saves
    
    * adds best-sales component
    
    * fixes app after merge conflicts
    
    * some fixes in input
    
    * fixes padding

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { SliderComponent } from './pages/slider/slider.component';
-import { CardComponent } from './shared/components/card/card.component';
+import { CardProductComponent } from './shared/components/card-product/card-product.component';
 import { AdvertisementComponent } from './shared/components/advertisement/advertisement.component';
 import { SaleComponent } from './shared/components/sale/sale.component';
+import { BestSalesComponent } from './pages/best-sales/best-sales.component';
+import { CardProductSmallComponent } from './shared/components/card-product-small/card-product-small.component';
 import { NewArrivalsComponent } from './pages/new-arrivals/new-arrivals.component';
 import { NewsLetterComponent } from './shared/components/news-letter/news-letter.component';
 
@@ -18,9 +20,11 @@ import { NewsLetterComponent } from './shared/components/news-letter/news-letter
     HeaderComponent,
     FooterComponent,
     SliderComponent,
-    CardComponent,
+    CardProductComponent,
     AdvertisementComponent,
     SaleComponent,
+    BestSalesComponent,
+    CardProductSmallComponent,
     NewArrivalsComponent,
     NewsLetterComponent,
   ],
