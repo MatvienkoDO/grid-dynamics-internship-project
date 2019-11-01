55ce2e74f01fd27cf9d20b98da86033ca092a914
DMatvienko <dmatvienko@griddynamics.com>
2019-11-01 21:31:06 +0400

    feat: creates models and removes redundant gitkeep

---
+++ frontend/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  brand: string;
+  price: number;
+  sizes: string[];
+  colors: string[];
+  images: string[];
+}
