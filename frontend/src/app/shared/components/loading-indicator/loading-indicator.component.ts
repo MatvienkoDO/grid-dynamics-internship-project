7d41a89c47c550db6eecd74abb9dc52ac6fc6bc5
DMatvienko <dmatvienko@griddynamics.com>
2019-11-01 23:39:09 +0400

    feat: creates loading indicator with ability to show error message

---
+++ frontend/src/app/shared/components/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  templateUrl: './loading-indicator.component.html',
+  styleUrls: ['./loading-indicator.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoadingIndicatorComponent implements OnInit {
+  @Input('show') show = true;
+  @Input('error-message') errorMessage = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
