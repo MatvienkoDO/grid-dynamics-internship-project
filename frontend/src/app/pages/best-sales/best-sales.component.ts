73fa5641d98ebc4079fb4c67b0f362ad176b9219
DMatvienko <dmatvienko@griddynamics.com>
2019-11-04 23:47:34 +0400

    feat: makes best sales a smart

--- frontend/src/app/pages/best-sales/best-sales.component.ts
+++ frontend/src/app/pages/best-sales/best-sales.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { Subscription, Subject } from 'rxjs';
+
+import { ProductsService } from 'src/app/shared/services';
+import { Product } from 'src/app/shared/models';
+import { CardProduct } from 'src/app/shared/models/card-product';
 
 @Component({
   selector: 'app-best-sales',
   templateUrl: './best-sales.component.html',
-  styleUrls: ['./best-sales.component.scss']
+  styleUrls: ['./best-sales.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BestSalesComponent implements OnInit {
-  constructor() { }
+export class BestSalesComponent implements OnInit, OnDestroy {
+  public readonly products$ = new Subject<Product[]>();
+  private subscription?: Subscription = undefined;
+
+  constructor(private readonly productsService: ProductsService) { }
 
   ngOnInit() {
+    this.subscription = this.productsService.getProducts(0, 3).subscribe({
+      next: (products: Product[]) => {
+        this.subscription.unsubscribe();
+        this.products$.next(products);
+      }
+    });
   }
 
+  ngOnDestroy() {
+    if (this.subscription && !this.subscription.closed) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  public readonly addProductToCart = (productInfo: CardProduct) => {
+    // TODO: realize addition to cart
+    alert(`It is not realized yet. Product id: ${productInfo.id}`);
+  }
 }
