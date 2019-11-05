434a32dfb52bf1c0b976ca31b2c5601bf04485ad
DMatvienko <dmatvienko@griddynamics.com>
2019-11-05 16:56:52 +0400

    feat: replaces slider to shared

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { SliderComponent } from './pages/slider/slider.component';
+import { SliderComponent } from './shared/components/slider/slider.component';
 import { CardProductComponent } from './shared/components/card-product/card-product.component';
 import { AdvertisementComponent } from './shared/components/advertisement/advertisement.component';
 import { SaleComponent } from './shared/components/sale/sale.component';
