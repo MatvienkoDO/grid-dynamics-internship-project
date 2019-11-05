21a60f19db9877ed5d69a2b278f67110eb907658
DMatvienko <dmatvienko@griddynamics.com>
2019-11-05 16:31:45 +0400

    feat: replaces best-sales component to shared folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { SliderComponent } from './pages/slider/slider.component';
 import { CardProductComponent } from './shared/components/card-product/card-product.component';
 import { AdvertisementComponent } from './shared/components/advertisement/advertisement.component';
 import { SaleComponent } from './shared/components/sale/sale.component';
-import { BestSalesComponent } from './pages/best-sales/best-sales.component';
+import { BestSalesComponent } from './shared/components/best-sales/best-sales.component';
 import { CardProductSmallComponent } from './shared/components/card-product-small/card-product-small.component';
 import { NewArrivalsComponent } from './pages/new-arrivals/new-arrivals.component';
 import { NewsLetterComponent } from './shared/components/news-letter/news-letter.component';
