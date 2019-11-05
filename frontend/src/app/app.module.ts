49120fbaba063ce0b86d8433837761730cc32b4a
DMatvienko <dmatvienko@griddynamics.com>
2019-11-05 17:03:06 +0400

    feat: creates barrel for shared components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { SliderComponent } from './shared/components/slider/slider.component';
-import { CardProductComponent } from './shared/components/card-product/card-product.component';
-import { AdvertisementComponent } from './shared/components/advertisement/advertisement.component';
-import { SaleComponent } from './shared/components/sale/sale.component';
-import { BestSalesComponent } from './shared/components/best-sales/best-sales.component';
-import { CardProductSmallComponent } from './shared/components/card-product-small/card-product-small.component';
-import { NewArrivalsComponent } from './shared/components/new-arrivals/new-arrivals.component';
-import { NewsLetterComponent } from './shared/components/news-letter/news-letter.component';
-import { ProductShowComponent } from './shared/components/product-show/product-show.component';
-import { LoadingIndicatorComponent } from './shared/components/loading-indicator/loading-indicator.component';
 import { HomeComponent } from './pages/home/home.component';
+
+import {
+  AdvertisementComponent,
+  BestSalesComponent,
+  CardProductComponent,
+  CardProductSmallComponent,
+  LoadingIndicatorComponent,
+  NewArrivalsComponent,
+  NewsLetterComponent,
+  ProductShowComponent,
+  SaleComponent,
+  SliderComponent,
+} from './shared/components';
 
 @NgModule({
   declarations: [
