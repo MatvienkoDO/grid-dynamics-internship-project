7a66dffb2ff9a5163d147fc700bdf9b7c04df1d6
DMatvienko <dmatvienko@griddynamics.com>
2019-11-06 15:12:30 +0400

    fix: fixes typo

--- backend/src/modules/product/product.controller.ts
+++ backend/src/modules/product/product.controller.ts
@@ -36,7 +36,7 @@ export class ProductController {
       response.data = await product;
     } catch (err) {
       // Mongoose returns an error with name CastError when document not found
-      if (err.name = 'CastError') {
+      if (err.name === 'CastError') {
         throw new HttpException('Resource not found', HttpStatus.NOT_FOUND);
       } else {
         throw new Error(err);
@@ -52,7 +52,7 @@ export class ProductController {
       response.data = await this.productService.update(id, updateProductDto);
     } catch (err) {
       // Mongoose returns an error with name CastError when document not found
-      if (err.name = 'CastError') {
+      if (err.name === 'CastError') {
         throw new HttpException('Resource not found', HttpStatus.NOT_FOUND);
       } else {
         throw new Error(err);
