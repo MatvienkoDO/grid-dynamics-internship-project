2c258b9fdec5fa8ca3a0d28df119464e8cfa071e
DMatvienko <dmatvienko@griddynamics.com>
2019-11-06 19:45:20 +0400

    feat: adds logic to slider

--- frontend/src/app/shared/components/slider/slider.component.ts
+++ frontend/src/app/shared/components/slider/slider.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { map, share } from 'rxjs/operators';
+
+import { ProductsService } from '../../services';
+import { Product } from '../../models';
 
 @Component({
   selector: 'app-slider',
@@ -7,8 +12,28 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SliderComponent implements OnInit {
-  constructor() { }
+  public readonly current$ = new BehaviorSubject<number>(0);
+  public currentProduct$: Observable<Product>;
+  public products$: Observable<Product[]>;
+
+  constructor(private readonly productsService: ProductsService) { }
 
   ngOnInit() {
+    this.products$ = this.productsService.getProductsForSlider()
+      .pipe(map(response => {
+        if (response && Array.isArray(response.data)) {
+          return response.data;
+        }
+
+        throw 'incorrect data';
+      }))
+      .pipe(share());
+
+    this.currentProduct$ = combineLatest(this.current$, this.products$)
+      .pipe(map(([slideNumber, products]) => products[slideNumber]));
+  }
+
+  public readonly changeSlide = (newCurrent: number) => {
+    this.current$.next(newCurrent);
   }
 }
