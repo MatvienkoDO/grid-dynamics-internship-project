cfffd33fae76fad86b90075c4a2cef656e27a3ac
DMatvienko <dmatvienko@griddynamics.com>
2019-11-07 13:05:17 +0400

    fix: removes incorrect things

--- frontend/src/app/shared/components/product-show/product-show.component.ts
+++ frontend/src/app/shared/components/product-show/product-show.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CardProductComponent } from 'src/app/shared/components/card-product/card-product.component';
-
 
 @Component({
   selector: 'app-product-show',
@@ -8,11 +6,8 @@ import { CardProductComponent } from 'src/app/shared/components/card-product/car
   styleUrls: ['./product-show.component.scss']
 })
 export class ProductShowComponent implements OnInit {
-  @Input() model: CardProductComponent;  
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }
