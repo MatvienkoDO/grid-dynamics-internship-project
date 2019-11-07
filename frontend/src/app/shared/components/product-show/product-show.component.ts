157279d77a8564bbe980d2e6d3ee21908324af67
DMatvienko <dmatvienko@griddynamics.com>
2019-11-07 13:06:06 +0400

    feat: turns on onpush in product show

--- frontend/src/app/shared/components/product-show/product-show.component.ts
+++ frontend/src/app/shared/components/product-show/product-show.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-product-show',
   templateUrl: './product-show.component.html',
-  styleUrls: ['./product-show.component.scss']
+  styleUrls: ['./product-show.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductShowComponent implements OnInit {
   constructor() { }
