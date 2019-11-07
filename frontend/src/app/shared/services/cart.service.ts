3ae95e1d2ee42a43a2a307de9760e88f3c9853e7
Timur <dkatimullin@gmail.com>
2019-11-07 11:00:34 +0400

    Adds cart service.

--- frontend/src/app/shared/services/cart.service.ts
+++ frontend/src/app/shared/services/cart.service.ts
@@ -1,28 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import { CardProduct } from '../models/card-product';
+
+const localStorageCartKey = 'CART_ITEMS';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  static readonly localStorageKey = 'cartItems';
-  items: CardProduct[] = [];
+  public items$: Observable<CardProduct[]>;
+
+  private items: BehaviorSubject<CardProduct[]>;
+  
   constructor() {}
 
+  ngOnInit() {
+    const itemsFromLocalStorage = this.getItemsFromLocalStorage();
+    const items = itemsFromLocalStorage || [];
+
+    this.items = new BehaviorSubject(items);
+    this.items$ = this.items;
+  }
+
   addToCart(cardProduct: CardProduct) {
-    this.items.push(cardProduct);
-    localStorage.setItem(CartService.localStorageKey, JSON.stringify(this.items));
-    console.log(this.getItems());
+    const updatedItems = [
+      ...this.items.value,
+      cardProduct
+    ];
+
+    this.saveItemsToLocalStorage(updatedItems);
+
+    this.items.next(updatedItems);
   }
 
-  getItems() {
-    return JSON.parse(localStorage.getItem(CartService.localStorageKey));
+  private getItemsFromLocalStorage(): CardProduct[] | null {
+    const localStorageData = localStorage.getItem(localStorageCartKey);
+
+    try {
+      const data = JSON.parse(localStorageData);
+
+      if (Array.isArray(data)) {
+        return data;
+      }
+    } catch (e) {
+      return null;
+    }
+
+    return null;
   }
 
-  clearCart() {
-    this.items = [];
-    localStorage.removeItem(CartService.localStorageKey);
-    return this.items;
+  private saveItemsToLocalStorage(items: CardProduct[]): void {
+    localStorage.setItem(localStorageCartKey, JSON.stringify(items));
   }
 }
