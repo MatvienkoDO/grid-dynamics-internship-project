f1c4ea6cf312776fb983f51b5dd6bbed149368c9
linkouth <34433986+linkouth@users.noreply.github.com>
2019-11-08 09:27:04 -0800

    Fix/pr 89 (#118)
    * Fixes backend comments.
    
    * Fixes backend comments.
    
    * fix: small fixes

--- backend/src/modules/product/product.response.ts
+++ backend/src/modules/product/product.response.ts
@@ -3,4 +3,9 @@ import { Product } from './product.interface'
 export class ProductResponse {
   public data: Product | Product[];
   public quantity: number;
+
+  constructor(data?: Product | Product[], quantity?: number) {
+    this.data = data;
+    this.quantity = quantity;
+  }
 }
--- backend/src/modules/test/test.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClothesController } from './controllers/clothes/clothes.controller';
-import { ClothesService } from './services/clothes/clothes.service';
-
-@Module({
-  controllers: [ClothesController],
-  providers: [ClothesService]
-})
-export class TestModule {}
