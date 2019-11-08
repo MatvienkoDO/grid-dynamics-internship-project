5a570a1772ae35691c9cf58db8d3820bf70090b3
Timur <dkatimullin@gmail.com>
2019-11-08 16:44:10 +0400

    Fixes backend comments.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppController } from './app.controller';
-import { ClothesController } from './clothes/clothes.controller';
 import { AppService } from './app.service';
-import { TestModule } from './modules/test/test.module';
 import { ProductModule } from './modules/product/product.module';
 import { databaseUri } from './database'
 import { NewsLetterModule } from './modules/news-letter/news-letter.module';
@@ -13,12 +10,10 @@ import { NewsLetterModule } from './modules/news-letter/news-letter.module';
 @Module({
   imports: [
     MongooseModule.forRoot(databaseUri),
-    AppRoutingModule,
-    TestModule,
     ProductModule,
     NewsLetterModule
   ],
-  controllers: [AppController, ClothesController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
