5d6507956a5e525080a9875ae00981a72baea280
DMatvienko <dmatvienko@griddynamics.com>
2019-11-12 03:41:30 +0400

    feat: realization of list-select component

--- frontend/src/app/shared/components/list-select/list-select.component.ts
+++ frontend/src/app/shared/components/list-select/list-select.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Option {
+  title: string;
+  value: string;
+}
+
+export type Options = Option[];
 
 @Component({
   selector: 'app-list-select',
@@ -7,10 +22,45 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListSelectComponent implements OnInit {
+  @Input() title = '';
+  @Input() options: Options = [];
+  @Input('one-of-many') oneOfMany = true;
+
+  @Output() values = new EventEmitter<string[]>();
+
+  public readonly selected$ = new BehaviorSubject<string[]>([]);
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  click(value: string) {
+    if (this.oneOfMany) {
+      this.oneOfManyClick(value);
+    } else {
+      this.aFewOfManyClick(value);
+    }
   }
 
+  private oneOfManyClick(value: string) {
+    const current: string | undefined = this.selected$.value[0];
+
+    const newValues = current === value
+      ? []
+      : [value];
+    
+    this.selected$.next(newValues);
+    this.values.emit(newValues);
+  }
+
+  private aFewOfManyClick(value: string) {
+    const current = this.selected$.value;
+
+    const newValues = current.indexOf(value) !== -1
+      ? current.filter(v => v !== value)
+      : current.concat([value]);
+
+    this.selected$.next(newValues);
+    this.values.emit(newValues);
+  }
 }
