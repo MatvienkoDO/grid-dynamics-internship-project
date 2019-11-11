2b87dfe7b8a58cfef1767bdbde13511d65b5764a
Timur <dkatimullin@gmail.com>
2019-11-11 11:10:59 +0400

    Adds filter .

--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './product.interface';
 import { ProductDto } from './dto/product.dto';
+import { Filter } from './filter.model';
 
 @Injectable()
 export class ProductService {
@@ -13,12 +14,26 @@ export class ProductService {
     return await createdCat.save();
   }
 
-  async findAll(skip: number = 0, limit: number = 4): Promise<Product[]> {
-    return await this.productModel.find(
-      null,
-      null,
-      { skip, limit }
-    );
+  async findAll(skip: number = 0, limit: number = 4, filter?: Filter): Promise<Product[]> {
+    const query = this.productModel.find(null, null, { skip, limit });
+    if (filter) {
+      if (filter.category) {
+        query.where('category').equals(filter.category);
+      }
+      if (Number.isInteger(filter.minPrice)) {
+        query.where('price').gte(filter.minPrice);
+      }
+      if (Number.isInteger(filter.maxPrice)) {
+        query.where('price').lte(filter.maxPrice);
+      }
+      if (filter.brands) {
+        query.where('brand').in(filter.brands);
+      }
+      if (filter.sizes) {
+        query.where('sizes').elemMatch({ $in: filter.sizes });
+      }
+    }
+    return await query;
   }
 
   async findById(id: string): Promise<Product> {
@@ -51,8 +66,26 @@ export class ProductService {
     return this.productModel.findById(id);
   }
 
-  async getCount(): Promise<number> {
-    return this.productModel.count({});
+  async getCount(filter?: Filter): Promise<number> {
+    const query = this.productModel.count(null);
+    if (filter) {
+      if (filter.category) {
+        query.where('category').equals(filter.category);
+      }
+      if (Number.isInteger(filter.minPrice)) {
+        query.where('price').gte(filter.minPrice);
+      }
+      if (Number.isInteger(filter.maxPrice)) {
+        query.where('price').lte(filter.maxPrice);
+      }
+      if (filter.brands) {
+        query.where('brand').in(filter.brands);
+      }
+      if (filter.sizes) {
+        query.where('sizes').elemMatch({ $in: filter.sizes });
+      }
+    }
+    return query;
   }
 }
 
