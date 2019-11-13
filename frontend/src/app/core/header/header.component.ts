dc3c5b00433e561001677021001ca2ca5e80d571
linkouth <34433986+linkouth@users.noreply.github.com>
2019-11-13 01:04:29 -0800

    Fixes closing mobile menu. (#139)
    * Fixes closing mobile menu.
    
    * Fixes comments.

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component,
+  OnInit,
+  ElementRef 
+} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { NotificationService } from '../../shared/services/notification/notification.service';
@@ -13,6 +17,7 @@ export class HeaderComponent implements OnInit {
   constructor(
     public readonly translate: TranslateService,
     private readonly notify: NotificationService,
+    private readonly elRef: ElementRef
   ) { }
 
   ngOnInit() {
@@ -23,4 +28,8 @@ export class HeaderComponent implements OnInit {
     this.translate.use(browserLang.match(/en|ru/) ? browserLang : 'en');
   }
 
+  onClickMenuItem() {
+    const closeButton = this.elRef.nativeElement.querySelector('.mobile-menu__btn');
+    closeButton.click();
+  }
 }
