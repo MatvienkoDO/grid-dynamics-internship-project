81a1afaafe186c76150af520dddd2918b780534d
jkozeeva <jkozeeva@griddynamics.com>
2019-11-14 21:35:48 +0400

    adds best-sales tests

--- frontend/src/app/shared/components/best-sales/best-sales.component.spec.ts
+++ frontend/src/app/shared/components/best-sales/best-sales.component.spec.ts
@@ -1,16 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockTranslatePipe } from '../../../testing/mock-translate.pipe';
+import { AppModule } from 'src/app/app.module';
+import { of } from 'rxjs';
 import { BestSalesComponent } from './best-sales.component';
+import { ProductsService } from '../../services';
+import { Product, CardProduct, ProductResponse } from 'src/app/shared/models';
 
 describe('BestSalesComponent', () => {
   let component: BestSalesComponent;
   let fixture: ComponentFixture<BestSalesComponent>;
+  const ProductsServiceSpy = jasmine.createSpyObj('ProductsService', ['getProducts']);
+  ProductsServiceSpy.getProducts.and.returnValue(of(<ProductResponse>{
+    data: [<Product>{id: '123'}],
+    quantity: 1
+  }));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BestSalesComponent ]
+      imports: [AppModule],
+      declarations: [MockTranslatePipe],
+      providers: [
+        { provide: ProductsService, useValue: ProductsServiceSpy },
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +34,13 @@ describe('BestSalesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#init should have products', () => {
+    expect(TestBed.get(ProductsService).getProducts).toHaveBeenCalledWith(0, 3);
+    component.products$.subscribe(value => {
+      expect(value).toEqual([<Product>{id: '123'}]);
+      // done();
+    });
   });
 });
