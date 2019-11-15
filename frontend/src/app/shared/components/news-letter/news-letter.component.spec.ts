292d7df5ebccc74b4cc38578f87fd4edaf3e0888
Timur <dkatimullin@gmail.com>
2019-11-15 13:39:27 +0400

    Adds new tests.

--- frontend/src/app/shared/components/news-letter/news-letter.component.spec.ts
+++ frontend/src/app/shared/components/news-letter/news-letter.component.spec.ts
@@ -2,14 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewsLetterComponent } from './news-letter.component';
 import { AppModule } from 'src/app/app.module';
+import { MailService } from '../../services';
+import { of } from 'rxjs';
 
 describe('NewsLetterComponent', () => {
   let component: NewsLetterComponent;
   let fixture: ComponentFixture<NewsLetterComponent>;
 
+  const mailServiceSpy = jasmine.createSpyObj('MailService', ['post']);
+  mailServiceSpy.post.and.returnValue(of({}));
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AppModule],
+      providers: [
+        { provide: MailService, useValue: mailServiceSpy },
+      ]
     })
     .compileComponents();
     fixture = TestBed.createComponent(NewsLetterComponent);
@@ -19,5 +27,12 @@ describe('NewsLetterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#onSubmit should call mailService.post', () => {
+    const stubEvent = new Event('stub');
+    expect(component.loading).toBe(false);
+    component.onSubmit(stubEvent);
+    expect(mailServiceSpy.post).toHaveBeenCalled();
   });
 });
