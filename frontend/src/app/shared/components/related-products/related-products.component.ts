5478b3837d2dad7964a54dfca4a7581223ecdf9a
Dmitriy Matvienko <chastor97@gmail.com>
2019-11-15 12:52:34 +0400

    Fix/cards buttons actions (#171)
    * fix: style fix
    
    * fix: removes console.log s
    
    * fix: adds actions to card buttons on products list
    
    * fix: adds button actions for card on pdp

--- frontend/src/app/shared/components/related-products/related-products.component.ts
+++ frontend/src/app/shared/components/related-products/related-products.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Product } from 'src/app/shared/models';
-import { ProductsService } from 'src/app/shared/services';
+import { Product, CardProduct } from 'src/app/shared/models';
+import { ProductsService, CartService, FavouritesService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-related-products[id]',
@@ -16,7 +17,12 @@ export class RelatedProductsComponent implements OnInit {
 
   public products$: Observable<Product[]>;
 
-  constructor(private readonly productsService: ProductsService) { }
+  constructor(
+    private readonly productsService: ProductsService,
+    private readonly router: Router,
+    private readonly cartService: CartService,
+    private readonly favouritesService: FavouritesService,
+  ) { }
 
   ngOnInit() {
     this.products$ = this.productsService.getRelatedProducts(this.id, 0, 4)
@@ -27,5 +33,17 @@ export class RelatedProductsComponent implements OnInit {
 
         throw 'incorrect data';
       }));
+  }
+
+  public readonly showDetails = (productId: string) => {
+    this.router.navigateByUrl(`/product/${productId}`);
+  }
+
+  public readonly addToCart = (cardProduct: CardProduct) => {
+    this.cartService.addToCart(cardProduct);
+  }
+
+  public readonly addToFavorite = (cardProduct: CardProduct) => {
+    this.favouritesService.addToFavourites(cardProduct);
   }
 }
