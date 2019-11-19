b06ae0dbce0f5418f3c388b2b5df316fe46dc4d7
DMatvienko <dmatvienko@griddynamics.com>
2019-11-19 16:20:32 +0400

    fix: creates mock for products service

--- frontend/src/app/pages/product-details/product-details.component.spec.ts
+++ frontend/src/app/pages/product-details/product-details.component.spec.ts
@@ -1,15 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { ProductDetailsComponent } from '..';
 import { AppModule } from 'src/app/app.module';
+import { ProductsService } from 'src/app/shared/services';
 
 describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
 
   beforeEach(() => {
+    const productsServiceMock = {
+      getProductById: () => of({})
+    };
+    
     TestBed.configureTestingModule({
       imports: [AppModule],
+      providers: [
+        { provide: ProductsService, useValue: productsServiceMock }
+      ],
     })
     .compileComponents();
     fixture = TestBed.createComponent(ProductDetailsComponent);
