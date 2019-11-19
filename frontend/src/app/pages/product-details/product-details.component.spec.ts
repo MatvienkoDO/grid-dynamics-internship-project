10a860ff5ab9a2e588393bb25bf47953a20c1d62
DMatvienko <dmatvienko@griddynamics.com>
2019-11-19 15:10:30 +0400

    fix: fixes import

--- frontend/src/app/pages/product-details/product-details.component.spec.ts
+++ frontend/src/app/pages/product-details/product-details.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductDetailsComponent } from './product-details.component';
+import { ProductDetailsComponent } from '..';
 import { AppModule } from 'src/app/app.module';
 
 describe('ProductDetailsComponent', () => {
