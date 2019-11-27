9c00a710f1a77142247c198b5f6e63d7618660b3
DMatvienko <dmatvienko@griddynamics.com>
2019-11-27 17:34:41 +0400

    feat: applies cookieParser with signing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
-import { mode, Mode } from './environment';
+import { mode, Mode, cookieSigningSecret } from './environment';
 
 async function bootstrap() {
   const modeName = mode === Mode.Production
@@ -11,6 +12,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cookieParser(cookieSigningSecret));
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
