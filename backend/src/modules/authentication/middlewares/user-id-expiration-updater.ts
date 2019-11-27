7e76f66c0f65d5d37c0cc50cab9ffdcc10d73955
DMatvienko <dmatvienko@griddynamics.com>
2019-11-27 19:14:14 +0400

    feat: clear cookie in case of incorrect

--- backend/src/modules/authentication/middlewares/user-id-expiration-updater.ts
+++ backend/src/modules/authentication/middlewares/user-id-expiration-updater.ts
@@ -4,8 +4,11 @@ import { userIdCookieKey, userIdCookieOptions } from '../common';
 
 export function userIdExpirationUpdater(request: Request, response: Response, next: NextFunction) {
   const userIdCookieValue = request.signedCookies[userIdCookieKey];
+  
   if (userIdCookieValue) {
     response.cookie(userIdCookieKey, userIdCookieValue, userIdCookieOptions);
+  } else {
+    response.clearCookie(userIdCookieKey);
   }
 
   next();
