12ebf32311798df25696b683810c924b18d75928
DMatvienko <dmatvienko@griddynamics.com>
2019-11-27 18:29:15 +0400

    feat: user id expiration updater

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 import { mode, Mode, cookieSigningSecret } from './environment';
+import { userIdExpirationUpdater } from './modules/authentication/middlewares/user-id-expiration-updater';
 
 async function bootstrap() {
   const modeName = mode === Mode.Production
@@ -13,6 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser(cookieSigningSecret));
+  app.use(userIdExpirationUpdater);
 
   await app.listen(process.env.PORT || 3000);
 }
---
+++ backend/src/modules/authentication/middlewares/user-id-expiration-updater.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { userIdCookieKey, userIdCookieOptions } from '../common';
+
+export function userIdExpirationUpdater(request: Request, response: Response, next: NextFunction) {
+  const userIdCookieValue = request.signedCookies[userIdCookieKey];
+  if (userIdCookieValue) {
+    response.cookie(userIdCookieKey, userIdCookieValue, userIdCookieOptions);
+  }
+
+  next();
+}
