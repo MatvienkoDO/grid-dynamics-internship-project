80593735fe51a630bb6a3a50e5714f432e439901
DMatvienko <dmatvienko@griddynamics.com>
2019-11-28 22:21:58 +0400

    feat: adds showing authentication dialog in case of unauth error

--- frontend/src/app/core/interceptors/error-handler.interseptor.ts
+++ frontend/src/app/core/interceptors/error-handler.interseptor.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import {
   HttpEvent,
   HttpInterceptor,
@@ -13,6 +14,8 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { NotificationService } from '../../shared/services';
+import { AccountComponent } from 'src/app/shared/components';
+import * as constants from '../../shared/constants';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -24,34 +27,54 @@ export class ErrorInterceptor implements HttpInterceptor {
   constructor(
     private readonly notificationService: NotificationService,
     private readonly router: Router,
+    private readonly dialog: MatDialog,
   ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
+        const info: any | null = error.error;
+
         if (error.status === 0) {
           this.notificationService.error(
             this.getNotificationServiceMessage('noInternet'));
+
         } else if (error.status === 404) {
           this.notificationService.error(
             this.getNotificationServiceMessage('notFound'));
+
+        } else if (info.status === constants.authFailedMessage) {
+          this.dialog.closeAll();
+          this.dialog.open(AccountComponent, AccountComponent.dialogConfig);
+          this.notificationService.warning(
+            this.getNotificationServiceMessage('youAreNotAuthenticated'));
+
         } else if (error.status >= 500 && error.status < 600) {
           this.notificationService.error(
             this.getNotificationServiceMessage('serverError'));
+
         } else {
           this.notificationService.error(
             this.getNotificationServiceMessage('unknownError'));
         }
+
         this.router.navigate(['']);
-        
+
         return throwError(error);
       })
     );
   }
 
   private getNotificationServiceMessage(type: string) {
-    const locale = localStorage.getItem('LOCALE');
-    return errorMessages[type][locale];
+    const locale = localStorage.getItem('LOCALE') || 'en';
+
+    const message = errorMessages[type];
+
+    if (message) {
+      return message[locale];
+    }
+
+    return '';
   }
 
 }
@@ -80,5 +103,9 @@ const errorMessages = {
   offlineNow: {
     en: 'You are offline now',
     ru: 'Соединение потеряно'
-  }
+  },
+  youAreNotAuthenticated: {
+    en: 'You are not authenticated',
+    ru: 'Вы не аутентифицированы',
+  },
 };
