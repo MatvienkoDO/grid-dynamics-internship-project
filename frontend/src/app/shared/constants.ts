7672c3e7d38ae924e8e6a2e716c17b4fd4e71906
DMatvienko <dmatvienko@griddynamics.com>
2019-11-28 20:38:11 +0400

    feat: adds auth failed message

--- frontend/src/app/shared/constants.ts
+++ frontend/src/app/shared/constants.ts
@@ -4,3 +4,5 @@ export const localStorageLocaleKey = 'LOCALE';
 
 export const passwordHashingPostfix =
   'PASSWORD_POSTFIX_97736483c4ba6b9b1556420';
+
+export const authFailedMessage = 'client_is_not_authorized';
