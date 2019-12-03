d3e801464efa0a533717b907295cc17593947dc0
DMatvienko <dmatvienko@griddynamics.com>
2019-12-03 15:05:10 +0400

    feat: sets samesite depending on configuration

--- backend/src/modules/authentication/common.ts
+++ backend/src/modules/authentication/common.ts
@@ -2,11 +2,14 @@ import { CookieOptions } from 'express';
 
 import { mode, Mode } from '../../environment';
 
+const isInProd = mode === Mode.Production;
+
 export const userIdCookieOptions: CookieOptions = {
   maxAge: 30 * 60 * 1000, // 30 mins
-  secure: mode === Mode.Production,
+  secure: isInProd,
   httpOnly: true,
   signed: true,
+  sameSite: isInProd ? 'None' : 'Lax',
 };
 
 export const userIdCookieKey = 'USER_ID';
