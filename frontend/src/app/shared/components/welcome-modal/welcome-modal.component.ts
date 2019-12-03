79772719bf45b035c88413e3a545549ba7aeba12
DMatvienko <dmatvienko@griddynamics.com>
2019-12-03 17:57:24 +0400

    fix: changes welcome component to load user data

--- frontend/src/app/shared/components/welcome-modal/welcome-modal.component.ts
+++ frontend/src/app/shared/components/welcome-modal/welcome-modal.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
-import { User } from '../../models/user';
-import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { User } from '../../models';
+import { AuthenticationService, UserService } from '../../services';
 
 @Component({
   selector: 'app-welcome-modal',
   templateUrl: './welcome-modal.component.html',
-  styleUrls: ['./welcome-modal.component.scss']
+  styleUrls: ['./welcome-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WelcomeModalComponent implements OnInit {
   public static readonly config: MatDialogConfig<WelcomeModalComponent> = {
     width: '550px',
   };
 
-  private readonly currentUserSubject: BehaviorSubject<User|null>;
-  currentUser$: Observable<User|null>;
+  public currentUser$: Observable<User>;
 
   constructor(
     private readonly dialogRef: MatDialogRef<WelcomeModalComponent>,
     private readonly authService: AuthenticationService,
-  ) {
-    this.currentUserSubject = new BehaviorSubject<User|null>(null);
-    this.currentUser$ = this.currentUserSubject.asObservable();
-  }
+    private readonly userService: UserService,
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentUser$ = this.userService.getUser();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
