6c3dc693c1f301e5386f2d36da864418e662865d
juliakozeeva <41913104+juliakozeeva@users.noreply.github.com>
2019-12-03 21:08:28 +0400

    Feature/frontend fixes error handling (#232)
    * fixes modal errors
    
    * adds eye for password
    
    * fixes invalid notifications
    
    * fixes error handling notifications

--- frontend/src/app/shared/services/account-modal/account-modal.service.ts
+++ frontend/src/app/shared/services/account-modal/account-modal.service.ts
@@ -29,7 +29,7 @@ export class AccountModalService {
   public openLoginSignup() {
     const dialogRef = this.dialog.open(AccountComponent, {
       width: '550px',
-      height: '580px',
+      height: '590px',
     });
     this.dialogStack.push(dialogRef);
   }
