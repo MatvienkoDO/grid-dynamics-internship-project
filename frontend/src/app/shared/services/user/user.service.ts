90ed29f5ca26f4f72d391bdaa03420622a9a5feb
DMatvienko <dmatvienko@griddynamics.com>
2019-12-05 12:58:03 +0400

    feat: adds common response model and fixes fe user service

--- frontend/src/app/shared/services/user/user.service.ts
+++ frontend/src/app/shared/services/user/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { User } from '../../models';
 import { apiHost } from 'src/environments';
+import { Response } from '../../models/response';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +18,17 @@ export class UserService {
 
   getMe(): Observable<User> {
     const url = `${apiHost}/api/users/me`;
-    return this.http.get<User>(url, { withCredentials: true });
+    return this.http.get<Response<User>>(url, { withCredentials: true })
+      .pipe(map(response => {
+        if (response && response.payload) {
+          return response.payload;
+        }
+
+        throw {
+          message: 'incorrect response',
+          payload: response,
+        }
+      }));
   }
 
 }
