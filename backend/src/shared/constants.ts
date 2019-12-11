a48eb62f3c88d58ba0b35f6c2d332dfea1549901
DMatvienko <dmatvienko@griddynamics.com>
2019-12-11 12:48:04 +0400

    fix: status for incorrect id

--- backend/src/shared/constants.ts
+++ backend/src/shared/constants.ts
@@ -3,7 +3,6 @@ import { CookieOptions } from 'express';
 import { mode, Mode } from '../environment';
 
 const isInProd = mode === Mode.Production;
-
 export const userIdCookieOptions: CookieOptions = {
   maxAge: 30 * 60 * 1000, // 30 mins
   secure: isInProd,
@@ -15,3 +14,4 @@ export const userIdCookieOptions: CookieOptions = {
 export const userIdCookieKey = 'USER_ID';
 
 export const unauthenticatedStatus = 'client_is_not_authorized';
+export const incorrectIdStatus = 'entity_with_such_id_is_not_found';
