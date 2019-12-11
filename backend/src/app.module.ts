f7bbd676c69cfe155b11595588363ccf956e3bb8
DMatvienko <dmatvienko@griddynamics.com>
2019-12-12 02:08:58 +0400

    fix: small linter fixes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/product/product.module';
-import { databaseUri } from './database'
+import { databaseUri } from './database';
 import { NewsLetterModule } from './modules/news-letter/news-letter.module';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
 import { UsersModule } from './modules/users/users.module';
