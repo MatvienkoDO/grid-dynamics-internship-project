507dce4ecc9685b3f6647d33c5af92eb762c3d79
juliakozeeva <41913104+juliakozeeva@users.noreply.github.com>
2019-12-12 09:20:25 +0400

    Feature/frontend account modal window (#265)
    * adds form for user profile
    
    * adds logic for buttons
    
    * fixes size of modal
    
    * fixes after mentors review
    
    * needs to do smth with colors imports

--- frontend/src/app/shared/components/welcome-modal/welcome-modal.component.ts
+++ frontend/src/app/shared/components/welcome-modal/welcome-modal.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 import { User } from '../../models';
 import { AuthenticationService, UserService, CartService } from '../../services';
+import { MustMatch } from '../../helpers/must-match.validator';
+import { AccountModalService } from '../../services/account-modal/account-modal.service';
 
 @Component({
   selector: 'app-welcome-modal',
@@ -15,18 +19,44 @@ export class WelcomeModalComponent implements OnInit {
   public static readonly config: MatDialogConfig<WelcomeModalComponent> = {
     width: '550px',
   };
-
+  private errorMessageSubject: BehaviorSubject<string>;
   public currentUser$: Observable<User>;
+
+  public submitted = false;
+  public isDisabled = true;
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
   constructor(
     private readonly dialogRef: MatDialogRef<WelcomeModalComponent>,
     private readonly authService: AuthenticationService,
     private readonly userService: UserService,
     private readonly cartService: CartService,
-  ) { }
+    private formBuilder: FormBuilder,
+    private readonly accountModalService: AccountModalService,
+  ) {
+    this.errorMessageSubject = new BehaviorSubject<string>('');
+  }
 
   ngOnInit() {
     this.currentUser$ = this.userService.getMe();
+
+    this.profileForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      });
   }
 
   onNoClick(): void {
@@ -39,4 +69,35 @@ export class WelcomeModalComponent implements OnInit {
     this.authService.logout();
     this.onNoClick();
   }
+
+  get profileFormControls() { return this.profileForm.controls; }
+
+  onSubmitprofileForm(event: Event) {
+    event.preventDefault();
+    this.submitted = true;
+    if (this.profileForm.invalid) {
+      return;
+    }
+    this.authService.signup(this.profileForm.value)
+      .pipe(
+        catchError(er => {
+          return of();
+        }),
+      )
+      .subscribe(
+        responseBody => {
+          if (responseBody && responseBody.status === 'error') {
+            this.errorMessageSubject.next(responseBody.message);
+          } else {
+            this.accountModalService.emptyDialogStack();
+            this.cartService.sendNewCartItems();
+            this.cartService.getCartItems();
+          }
+        });
+  }
+
+  formEnable(): void {
+    this.isDisabled = false;
+  }
+
 }
