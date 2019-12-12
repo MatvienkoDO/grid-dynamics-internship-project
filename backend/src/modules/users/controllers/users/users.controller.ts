5420701665dd54cf36ba7b8fc3a7b00ccdb597e0
linkouth <34433986+linkouth@users.noreply.github.com>
2019-12-12 09:35:52 -0800

    Feature/backend edit user (#269)
    * Fixes response quantity.
    
    * Fixes patch user.
    
    * Resolves conflicts.
    
    * Refactors.

--- backend/src/modules/users/controllers/users/users.controller.ts
+++ backend/src/modules/users/controllers/users/users.controller.ts
@@ -5,13 +5,16 @@ import {
   Request,
   Response,
   HttpStatus,
+  Patch,
+  Body,
 } from '@nestjs/common';
 import * as express from 'express';
 
 import { AuthGuard } from '../../../authentication/guards/auth/auth.guard';
 import { User } from '../../models/user';
 import { userIdCookieKey } from '../../../../shared/constants';
-import { UserService } from '../../services/user/user.service';
+import { UserService } from '../../services/user/user.service'
+import { EditUserDto } from '../../models/edit-user.dto';
 
 @Controller('api/users')
 export class UsersController {
@@ -46,4 +49,31 @@ export class UsersController {
     }
   }
 
+  @UseGuards(AuthGuard)
+  @Patch()
+  async editUser(
+    @Body() userDto: EditUserDto,
+    @Request() request: express.Request,
+    @Response() response: express.Response,
+  ) {
+    const userId: string = request.signedCookies[userIdCookieKey];
+    userDto.id = userId;
+    const validateResult = await this.usersService.validate(userDto);
+    if (validateResult.errors) {
+      response.status(HttpStatus.BAD_REQUEST);
+      response.send({
+        success: false,
+        status: 'invalid_edit_user_form',
+        errors: validateResult.errors,
+      });
+      
+      return;
+    }
+    const updatedUser = await this.usersService.update(userDto);
+
+    return response.send({
+      success: true,
+      payload: updatedUser,
+    });
+  }
 }
