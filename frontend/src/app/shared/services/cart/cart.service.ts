2d396e189404192d8944f24983081b156dad1ecb
DMatvienko <dmatvienko@griddynamics.com>
2019-12-16 19:15:18 +0400

    fix: usages of correct localization

--- frontend/src/app/shared/services/cart/cart.service.ts
+++ frontend/src/app/shared/services/cart/cart.service.ts
@@ -34,7 +34,7 @@ export class CartService {
     }
   }
 
-  public addToCart(cardProduct: CardProduct) {
+  public async addToCart(cardProduct: CardProduct) {
     const updatedItems = this.items.value;
 
     const idx = this.findIndexSameCardProduct(cardProduct);
@@ -48,7 +48,7 @@ export class CartService {
 
     this.items.next(updatedItems);
 
-    const message = this.localizationService.getNotificationServiceMessage('addToCart');
+    const message = await this.localizationService.get('cart.addToCart').toPromise();
     this.notificationService.info(`${cardProduct.title} ${message}`);
   }
 
@@ -63,7 +63,7 @@ export class CartService {
     return -1;
   }
 
-  public deleteFromCart(cardProduct: CardProduct) {
+  public async deleteFromCart(cardProduct: CardProduct) {
     const updatedItems = [
       ...this.items.value.filter(el => el.id !== cardProduct.id)
     ];
@@ -73,11 +73,11 @@ export class CartService {
     this.items.next(updatedItems);
     this.updateCartItems();
 
-    const message = this.localizationService.getNotificationServiceMessage('deleteFromCart');
+    const message = await this.localizationService.get('cart.deleteFromCart').toPromise();
     this.notificationService.warning(`${cardProduct.title} ${message}`);
   }
 
-  public clearCart() {
+  public async clearCart() {
     const updatedItems = [];
 
     this.saveItemsToLocalStorage(updatedItems);
@@ -85,7 +85,7 @@ export class CartService {
     this.items.next(updatedItems);
     this.updateCartItems();
 
-    const message = this.localizationService.getNotificationServiceMessage('clearCart');
+    const message = await this.localizationService.get('cart.clearCart').toPromise();
     this.notificationService.warning(message);
   }
 
@@ -145,7 +145,6 @@ export class CartService {
     const address = `${apiHost}/api/cart`;
     const body = { items: this.items.getValue() };
     const options = { withCredentials: true };
-
     const debounce = 1000;
 
     return this.http.put<any>(address, body, options)
