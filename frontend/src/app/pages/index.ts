d279289517baa8871396d24bcdbc0c2939a4e582
juliakozeeva <41913104+juliakozeeva@users.noreply.github.com>
2019-12-17 10:23:13 +0400

    Feature/frontend about page (#287)
    * adds about page in routing
    
    * adds about page
    
    * adds translate
    
    * fixes

--- frontend/src/app/pages/index.ts
+++ frontend/src/app/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './home/home.component';
 export * from './product-details/product-details.component';
 export * from './products/products.component';
+export * from './about/about.component';
