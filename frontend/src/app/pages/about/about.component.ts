a67fe734aa9465cfa7964a27385470dce1011154
jkozeeva <jkozeeva@griddynamics.com>
2019-12-17 10:22:51 +0400

    fixes

--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -5,11 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class AboutComponent { }
