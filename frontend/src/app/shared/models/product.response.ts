3b1e6fd8a34ee1190f0a99633322099541218b14
DMatvienko <dmatvienko@griddynamics.com>
2019-12-26 17:34:46 +0400

    fix: product response refactoring

--- frontend/src/app/shared/models/product.response.ts
+++ frontend/src/app/shared/models/product.response.ts
@@ -1,13 +1,8 @@
-import { Product } from './product'
+import { Product } from './product';
 
 export class ProductResponse {
-  public data: Product | Product[];
-  public quantity: number;
-
-  constructor(data?: Product | Product[], quantity?: number) {
-    if (data && quantity) {
-      this.data = data;
-      this.quantity = quantity;
-    }
-  }
+  constructor(
+    public readonly data?: Product | Product[],
+    public readonly quantity?: number,
+  ) { }
 }
