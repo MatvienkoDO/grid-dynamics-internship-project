20d77d74d1b59186a5bebbdb5ac43acd77c24002
Timur <dkatimullin@gmail.com>
2020-01-15 08:11:36 +0400

    Refactors.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true
 };
 
-// export const apiHost = 'https://gd-internship-ng-server.herokuapp.com';
-export const apiHost = 'http://localhost:3000';
+export const apiHost = 'https://gd-internship-ng-server.herokuapp.com';
 
 export const googleMapEmbedKey = 'AIzaSyB1av8wRYCxNNKQ7sNwYllTlJG-jbrpo4M';
