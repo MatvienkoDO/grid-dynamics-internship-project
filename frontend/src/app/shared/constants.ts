d04b9b028c0590b22846d7d7021c18e1ffd1d12c
juliakozeeva <41913104+juliakozeeva@users.noreply.github.com>
2020-01-16 15:17:24 +0400

    adds greek translation (#320)
    * adds greek translation
    
    * adds french translation
    
    * adds constant for array of language
    
    * fixes styles
    
    * fixes styles

--- frontend/src/app/shared/constants.ts
+++ frontend/src/app/shared/constants.ts
@@ -4,6 +4,8 @@ export const localStorageCartKey = 'CART_ITEMS';
 export const localStorageFavouritesKey = 'FAVOURITES_ITEMS';
 export const localStorageLocaleKey = 'LOCALE';
 
+export const langs = ['en', 'ru', 'fr', 'gr'];
+
 export const authFailedMessage = 'client_is_not_authorized';
 export const signupInvalidForm = 'signup_invalid_form';
 export const emailIsNotUnique = 'email_is_not_unique';
